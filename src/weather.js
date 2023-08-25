a5eb648777ab54af662b8a270005a77494c9d06e
Vincent0103 <vincentkanagasundaram@gmail.com>
2023-08-25 15:03:07 +0200

    Organize code to make it more readable

--- src/weather.js
+++ src/weather.js
@@ -20,45 +20,61 @@ const weatherLogic = (() => {
 })();
 
 const weatherDOM = (() => {
-  function changeWeatherData(weatherDataContainer, weatherDataObj) {
-    function handleWeatherIcon(condition, container) {
-      const weatherIconContainer = container;
-      if (weatherIconContainer.querySelector('svg')) {
-        weatherIconContainer.querySelector('svg').remove();
-      } else if (weatherIconContainer.querySelector('img')) {
-        weatherIconContainer.querySelector('img').remove();
-      }
-
-      const mySvg = new Image();
-      mySvg.classList.add('weather-icon');
-      if (condition.includes('rain')) mySvg.src = rainyIcon;
-      else if (condition.includes('cloud')) mySvg.src = cloudyIcon;
-      else if (condition.includes('sunny')) mySvg.src = sunnyIcon;
-      weatherIconContainer.appendChild(mySvg);
+  function handleWeatherIcon(condition, container) {
+    const weatherIconContainer = container;
+    if (weatherIconContainer.querySelector('svg')) {
+      weatherIconContainer.querySelector('svg').remove();
+    } else if (weatherIconContainer.querySelector('img')) {
+      weatherIconContainer.querySelector('img').remove();
     }
 
+    const mySvg = new Image();
+    mySvg.classList.add('weather-icon');
+    if (condition.includes('rain')) mySvg.src = rainyIcon;
+    else if (condition.includes('cloud')) mySvg.src = cloudyIcon;
+    else if (condition.includes('sunny')) mySvg.src = sunnyIcon;
+    weatherIconContainer.appendChild(mySvg);
+  }
+
+  function changeHourLocationWeatherData(weatherDataContainer, weatherDataObj) {
     const h1 = weatherDataContainer.querySelector('.location-heading');
+
     const cityHeading = h1.querySelector('.city-heading');
-    const countryHeading = h1.querySelector('.country-heading');
-    const avgTempHeading = weatherDataContainer.querySelector('.temp-heading');
-    const tempConditioniconHeading = weatherDataContainer.querySelector('.temp-conditionicon-heading');
-    const conditionHeading = weatherDataContainer.querySelector('.condition-heading');
-    const minTempP = weatherDataContainer.querySelector('.min-temp-container > p');
-    const maxTempP = weatherDataContainer.querySelector('.max-temp-container > p');
-    const airQualityP = weatherDataContainer.querySelector('.air-quality-container > p');
-    const humidityP = weatherDataContainer.querySelector('.humidity-container > p');
     cityHeading.textContent = `${weatherDataObj.city}, `;
+
+    const countryHeading = h1.querySelector('.country-heading');
     countryHeading.textContent = weatherDataObj.country;
+
+    const avgTempHeading = weatherDataContainer.querySelector('.temp-heading');
     avgTempHeading.textContent = `${weatherDataObj.avgTemp.celsius}°C`;
+
+    const tempConditioniconHeading = weatherDataContainer.querySelector('.temp-conditionicon-heading');
     handleWeatherIcon(weatherDataObj.condition, tempConditioniconHeading);
 
+    const conditionHeading = weatherDataContainer.querySelector('.condition-heading');
     conditionHeading.textContent = weatherDataObj.condition.slice(0, 1).toUpperCase()
     + weatherDataObj.condition.slice(1);
 
+    const minTempP = weatherDataContainer.querySelector('.min-temp-container > p');
     minTempP.textContent = `${weatherDataObj.minTemp.celsius}°C`;
+
+    const maxTempP = weatherDataContainer.querySelector('.max-temp-container > p');
     maxTempP.textContent = `${weatherDataObj.maxTemp.celsius}°C`;
+
+    const airQualityP = weatherDataContainer.querySelector('.air-quality-container > p');
     airQualityP.textContent = `${weatherDataObj.airQuality} CO`;
+
+    const humidityP = weatherDataContainer.querySelector('.humidity-container > p');
     humidityP.textContent = weatherDataObj.avgHumidity;
+  }
+
+  function changeForecastLocationWeatherData(forecastDataContainer, forecastDataObj, forecastNum) {
+    const tempHeading = forecastDataContainer.querySelector('.temp-heading');
+    tempHeading.textContent = forecastDataObj[forecastNum].day.avgtemp_c;
+  }
+
+  function changeWeatherData(weatherDataContainer, weatherDataObj) {
+    changeHourLocationWeatherData(weatherDataContainer, weatherDataObj);
   }
 
   async function getWeatherCityDataObj(location) {
@@ -73,6 +89,7 @@ const weatherDOM = (() => {
 
     const currentHour = weatherLocationData.current;
     const forecastDay = weatherLocationData.forecast.forecastday[0];
+    const forecastDays = weatherLocationData.forecast.forecastday;
     const avgTemp = {
       celsius: Math.round(currentHour.temp_c),
       fahrenheit: Math.round(currentHour.temp_f),
@@ -90,7 +107,7 @@ const weatherDOM = (() => {
     const avgHumidity = Math.round(currentHour.humidity);
 
     return {
-      city, country, avgTemp, minTemp, maxTemp, condition, airQuality, avgHumidity,
+      city, country, avgTemp, minTemp, maxTemp, condition, airQuality, avgHumidity, forecastDays,
     };
   }
 
@@ -110,6 +127,7 @@ const weatherDOM = (() => {
 
   function content() {
     const allForecastContainer = document.querySelector('.allForecast-location-weather');
+    const forecastLocationWeather = document.querySelector('.forecast-location-weather');
     const weatherPanelContainer = document.querySelector('.weather-panel-container');
     const weatherSearchContainer = document.querySelector('.weather-search-container');
     const weatherDataContainer = weatherPanelContainer.querySelector('.today-location-weather');
@@ -122,7 +140,9 @@ const weatherDOM = (() => {
       getWeatherCityDataObj(inputLocation.value)
         .then((response) => {
           const weatherDataObj = response;
+          const forecastDataObj = weatherDataObj.forecastDays;
           changeWeatherData(weatherDataContainer, weatherDataObj);
+          changeForecastLocationWeatherData(forecastLocationWeather, forecastDataObj, 0);
         });
     });
   }
@@ -135,3 +155,4 @@ const weatherDOM = (() => {
 })();
 
 export default weatherDOM;
+export const getWeatherDataFromLocationModule = weatherLogic.getWeatherDataFromLocation;
